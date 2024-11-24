1e0db3766b30e1067e6244416004d49dd5b47d40
Rayan <elkharr@cy-tech.fr>
2024-11-24 23:24:42 +0100

    refonte + fct draw_line + fct execute_all_command

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import tkinter as tk
 from text_edition import root, text_area, menu_commands, create_menus  # Importer les composants depuis text_edition.py
-from drawing_commands import open_drawing_window  # Importer les fonctions de dessin depuis drawing_commands.py
+from drawing_commands import open_drawing_window
 
-# Fonction pour exécuter les commandes de dessin
+
 def run_draw_commands():
     # Obtenir le texte de la zone d'édition et le diviser en lignes de commandes
     commands = text_area.get("1.0", tk.END).splitlines()
     open_drawing_window(commands)  # Ouvrir la fenêtre de dessin et interpréter les commandes
 
-# Démarrer l'interface
-root.mainloop()
 
+root.mainloop()
